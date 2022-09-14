049d6f80d6f821d3ccffbb679eff8ca95f2fa804
A T Curtis <atcurtis@gmail.com>
2022-09-14 04:12:18 -0700

    Fixup some node dependencies

--- view/scripts/webpack.prod.js
+++ view/scripts/webpack.prod.js
@@ -45,6 +45,35 @@ module.exports = {
         }
       },
       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              query: {
+                name:'assets/[name].[ext]'
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader'
       },
