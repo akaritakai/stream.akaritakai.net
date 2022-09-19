8795992490311d68a9e7b1aa5b9b6303521004ad
A T Curtis <atcurtis@gmail.com>
2022-09-19 10:59:18 -0700

    Start splitting the large Dashboard.vue
    new StreamViewers vue

--- view/src/dashboard/Dashboard.vue
+++ view/src/dashboard/Dashboard.vue
@@ -44,37 +44,7 @@
             </table>
           </b-tab>
           <b-tab title="Viewers">
-            <table class="table">
-              <tbody>
-              <tr>
-                <th>IP Address</th>
-                <th>Nick</th>
-                <th>Stream Size</th>
-                <th>Chat Opened</th>
-                <th>Window Visible</th>
-                <th>Full Screen</th>
-                <th>Stream Muted</th>
-                <th>Quality</th>
-                <th>Bandwidth</th>
-              </tr>
-              <template v-for="t in telemetry">
-                <tr>
-                  <td>{{ t.ipAddress }}</td>
-                  <td>{{ t.request.chatNick }}</td>
-                  <td>{{ t.request.streamWidth }}x{{ t.request.streamHeight }}</td>
-                  <td>{{ t.request.chatOpened }}</td>
-                  <td>{{ t.request.visible }}</td>
-                  <td>{{ t.request.videoFullScreen || t.request.pageFullScreen }}</td>
-                  <td>{{ t.request.muted }}</td>
-                  <td>{{ t.request.videoQuality ? t.request.videoQuality : "" }}</td>
-                  <td>{{ t.request.clientBandwidth ? (t.request.clientBandwidth > 10000000
-                    ? Math.round(t.request.clientBandwidth / 1000000) + " Mbps"
-                    : Math.round(t.request.clientBandwidth / 1000) + " Kbps")
-                    : "" }}</td>
-                </tr>
-              </template>
-              </tbody>
-            </table>
+            <stream-viewers :telemetry="telemetry"/>
           </b-tab>
           <b-tab title="Actions">
             <table class="table max100w">
@@ -246,11 +216,19 @@
   import videojs from 'video.js/core';
   import moment from 'moment';
 
+  const StreamViewers = () => import(
+    /* webpackChunkName: "streamViewers" */
+    /* webpackPrefetch: true */
+    './StreamViewers.vue');
+
   Vue.use(BootstrapVue);
   Vue.use(AlertPlugin);
 
   export default {
     name: 'Dashboard',
+    components: {
+      StreamViewers
+    },
     data() {
       return {
         form: {
---
+++ view/src/dashboard/StreamViewers.vue
@@ -0,0 +1,50 @@
+<template>
+  <table class="table">
+    <tbody>
+    <tr>
+      <th>IP Address</th>
+      <th>Nick</th>
+      <th>Stream Size</th>
+      <th>Chat Opened</th>
+      <th>Window Visible</th>
+      <th>Full Screen</th>
+      <th>Stream Muted</th>
+      <th>Quality</th>
+      <th>Bandwidth</th>
+    </tr>
+    <template v-for="t in telemetry">
+      <tr>
+        <td>{{ t.ipAddress }}</td>
+        <td>{{ t.request.chatNick }}</td>
+        <td>{{ t.request.streamWidth }}x{{ t.request.streamHeight }}</td>
+        <td>{{ t.request.chatOpened }}</td>
+        <td>{{ t.request.visible }}</td>
+        <td>{{ t.request.videoFullScreen || t.request.pageFullScreen }}</td>
+        <td>{{ t.request.muted }}</td>
+        <td>{{ t.request.videoQuality ? t.request.videoQuality : "" }}</td>
+        <td>{{ t.request.clientBandwidth ? (t.request.clientBandwidth > 10000000
+          ? Math.round(t.request.clientBandwidth / 1000000) + " Mbps"
+          : Math.round(t.request.clientBandwidth / 1000) + " Kbps")
+          : "" }}</td>
+      </tr>
+    </template>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'stream-viewers',
+    data () {
+      return { };
+    },
+    props: {
+      telemetry: null
+    }
+  }
+</script>
+
+<style lang="scss">
+  .stream-viewers {
+  }
+</style>
